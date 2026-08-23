a15a483390b38c2e1d74591fed2d2f5230669ceb
Wangic7 <wangic7@outlook.com>
2026-08-23 16:37:29 +0800

    Add protobuf serialization demo

--- src/protobuf_demo.cpp
+++ src/protobuf_demo.cpp
@@ -1,42 +1,58 @@
 #include <iostream>
 #include <string>
 
-#include "proto/calculator.pb.h"
+#include "../proto/calculator.pb.h"
 
 int main()
 {
-   //1.客户端创建请求对象
-	minirpc::AddRequest request;
+    minirpc::AddRequest request;
 
-	request.set_a(10);
-	request.set_b(20);
+    request.set_a(10);
+    request.set_b(20);
 
-	std::cout << "Before serialization:" << std::endl;
-	std::cout << "a = " << request.a() << std::endl;
-	std::cout << "b = " << request.b() << std::endl;
-	//2.将cpp对象序列化成字节数据
-	std::string data;
-	if(!request.SerializeToString(&data))
-      {
-         std::cerr << "Serialization failed!" << std::endl;
-	 return 1;
+    std::cout << "Before serialization:" << std::endl;
+    std::cout << "a = "
+              << request.a()
+              << std::endl;
 
-      }
-	std::cout << "Serialized size: "
-		  << data.size()
-		  << " bytes"
-		  <<std::endl;
-	//3.模拟服务器收到数据
-	minirpc::AddRequest received_request;
-	//4.将字节数据反序列化回cpp对象
-	if(!received_request.ParseFromString(data))
-        {
-		std::cerr << "Deserialization failed" << std::endl;
-		return 1;
-        }
-	std::cout << "After deserialization:" << std::endl;
-	std::cout << "a = " << received_request.a() <<std::endl;
-	std::cout << "b = " << received_request.b() <<std::endl;
+    std::cout << "b = "
+              << request.b()
+              << std::endl;
 
-	return 0;
+
+    std::string data;
+
+
+    if(request.SerializeToString(&data))
+    {
+        std::cout << "Serialize success!" << std::endl;
+
+        std::cout << "Data size: "
+                  << data.size()
+                  << std::endl;
+    }
+
+
+    minirpc::AddRequest new_request;
+
+
+    if(new_request.ParseFromString(data))
+    {
+        std::cout << "Deserialize success!"
+                  << std::endl;
+
+        std::cout << "a = "
+                  << new_request.a()
+                  << std::endl;
+
+        std::cout << "b = "
+                  << new_request.b()
+                  << std::endl;
+    }
+
+
+    return 0;
 }
+
+
+
